d7e2235ecc26ffb6f8dec257249eb06f23897e6a
xhinsade29 <lopezkristine@gmail.com>
2026-04-01 09:26:03 +0800

    error: restore the node_modules  dependencies

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -8,7 +8,6 @@ import {
   IonContent,
   IonToolbar,
   IonTitle,
-  IonPage,
   IonItem,
   IonInput,
 } from '@ionic/react';
@@ -33,46 +32,40 @@ const ExploreContainer: React.FC = () => {
   }
 
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Inline Modal</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonButton id="open-modal" expand="block">
-          Open
-        </IonButton>
-        <p>{message}</p>
-        <IonModal ref={modal} trigger="open-modal" onWillDismiss={(event) => onWillDismiss(event)}>
-          <IonHeader>
-            <IonToolbar>
-              <IonButtons slot="start">
-                <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
-              </IonButtons>
-              <IonTitle>Welcome</IonTitle>
-              <IonButtons slot="end">
-                <IonButton strong={true} onClick={() => confirm()}>
-                  Confirm
-                </IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">
-            <IonItem>
-              <IonInput
-                label="Enter your name"
-                labelPlacement="stacked"
-                ref={input}
-                type="text"
-                placeholder="Your name"
-              />
-            </IonItem>
-          </IonContent>
-        </IonModal>
-      </IonContent>
-    </IonPage>
+    <div className="ion-padding">
+      <IonButton id="open-modal" expand="block">
+        Open
+      </IonButton>
+      <p>{message}</p>
+      <IonModal ref={modal} trigger="open-modal" onWillDismiss={(event) => onWillDismiss(event)}>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+            </IonButtons>
+            <IonTitle>Welcome</IonTitle>
+            <IonButtons slot="end">
+              <IonButton strong={true} onClick={() => confirm()}>
+                Confirm
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonItem>
+            <IonInput
+              label="Enter your name"
+              labelPlacement="stacked"
+              ref={input}
+              type="text"
+              placeholder="Your name"
+            />
+          </IonItem>
+        </IonContent>
+      </IonModal>
+    </div>
   );
 };
 
 export default ExploreContainer;
+
